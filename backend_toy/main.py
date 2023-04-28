86b09ea8d3f446cac3dbc0a0bd3626823b62c969
김동민 <kimdongmin@gimdongmin-ui-noteubug.local>
2023-04-28 11:45:15 +0900

    third commit

--- backend_toy/main.py
+++ backend_toy/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return 'hello again!'
+    return 'hello again! again!'
 
 if __name__=="__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
