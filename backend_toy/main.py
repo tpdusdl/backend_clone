4abc72537cd5064d83f353c7b6fb45fbb68aeac8
KimDongmin <87116807+KimDongmin317@users.noreply.github.com>
2023-04-29 15:54:07 +0900

    8th commit

--- backend_toy/main.py
+++ backend_toy/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return 'hello7'
+    return 'hello8'
 
 if __name__=="__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
