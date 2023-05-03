a642a6d95d1b93f838de8598c298c2e16c68b98a
tpdusdl <74286434+tpdusdl@users.noreply.github.com>
2023-05-03 23:41:59 +0900

    16commit

--- backend_toy/main.py
+++ backend_toy/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return 'hello15'
+    return 'hello16'
 
 if __name__=="__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
